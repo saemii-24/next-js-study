9aeaada804494b933626fa853a83e58a8bf52fba
saemii-24 <hims2419@gmail.com>
2025-01-17 00:03:53 +0900

    Feat: sign 컴포넌트

--- components/TouchClick.tsx
+++ components/TouchClick.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, {useState, useRef} from 'react';
+import {cn} from 'utils/cn';
 
 const TouchClick = () => {
 	const [position, setPosition] = useState({x: 50, y: 50});
@@ -7,6 +8,15 @@ const TouchClick = () => {
 
 	const containerRef = useRef<HTMLDivElement>(null);
 	const buttonSize = 80; // 버튼 크기 (px)
+
+	// 이벤트 상태
+	const [isTouchStart, setTouchStart] = useState(false);
+	const [isTouchMove, setTouchMove] = useState(false);
+	const [isTouchEnd, setTouchEnd] = useState(false);
+	const [isTouchCancel, setTouchCancel] = useState(false);
+	const [isClick, setClick] = useState(false);
+	const [isMouseDown, setMouseDown] = useState(false);
+	const [isMouseUp, setMouseUp] = useState(false);
 
 	// 상태 및 로그 초기화
 	const clearLog = () => {
@@ -22,6 +32,11 @@ const TouchClick = () => {
 	) => {
 		event.preventDefault();
 		setIsDragging(true);
+		if ('touches' in event) {
+			setTouchStart(true); // touch 이벤트 시작 시
+		} else {
+			setMouseDown(true); // mouse 이벤트 시작 시
+		}
 	};
 
 	// 드래그 중
@@ -37,10 +52,12 @@ const TouchClick = () => {
 			// TouchEvent인 경우
 			clientX = event.touches[0].clientX;
 			clientY = event.touches[0].clientY;
+			setTouchMove(true); // 드래그 중 touch
 		} else {
 			// MouseEvent인 경우
 			clientX = event.clientX;
 			clientY = event.clientY;
+			setMouseDown(true); // 드래그 중 mouse
 		}
 
 		const container = containerRef.current;
@@ -62,8 +79,13 @@ const TouchClick = () => {
 	};
 
 	// 드래그 종료
-	const handleDragEnd = () => {
+	const handleDragEnd = (event: React.MouseEvent | React.TouchEvent) => {
 		setIsDragging(false);
+		if ('touches' in event) {
+			setTouchEnd(true); // touch 이벤트 종료 시
+		} else {
+			setMouseUp(true); // mouse 이벤트 종료 시
+		}
 	};
 
 	return (
@@ -72,10 +94,13 @@ const TouchClick = () => {
 			<div
 				ref={containerRef}
 				className='relative h-60 w-full rounded border border-gray-300 bg-gray-100'
+				onTouchStart={() => setTouchStart(true)}
+				onTouchMove={(e) => {
+					handleDragMove(e);
+				}}
 				onMouseMove={(e) => handleDragMove(e)}
-				onTouchMove={(e) => handleDragMove(e)}
 				onMouseUp={handleDragEnd}
-				onTouchEnd={handleDragEnd}
+				onTouchEnd={(e) => handleDragEnd(e)}
 				onMouseLeave={handleDragEnd}>
 				{/* 드래그 가능한 버튼 */}
 				<button
@@ -91,8 +116,50 @@ const TouchClick = () => {
 					onTouchStart={(e) => handleDragStart(e)}
 					className='rounded-md border border-gray-300 bg-white p-4 text-center transition hover:bg-blue-500'></button>
 			</div>
+			<ul className='mt-4'>
+				<li className='flex items-center gap-3'>
+					<Sign nowEvent={isTouchStart} />
+					onTouchStart
+				</li>
+				<li className='flex items-center gap-3'>
+					<Sign nowEvent={isTouchMove} />
+					onTouchMove
+				</li>
+				<li className='flex items-center gap-3'>
+					<Sign nowEvent={isTouchEnd} />
+					onTouchEnd
+				</li>
+				<li className='flex items-center gap-3'>
+					<Sign nowEvent={isTouchCancel} />
+					onTouchCancel
+				</li>
+				<li className='flex items-center gap-3'>
+					<Sign nowEvent={isClick} />
+					onClick
+				</li>
+				<li className='flex items-center gap-3'>
+					<Sign nowEvent={isMouseDown} />
+					onMouseDown
+				</li>
+				<li className='flex items-center gap-3'>
+					<Sign nowEvent={isMouseUp} />
+					onMouseUp
+				</li>
+			</ul>
 		</div>
 	);
 };
 
 export default TouchClick;
+
+const Sign = ({nowEvent}: {nowEvent: boolean}) => {
+	return (
+		<>
+			<div
+				className={cn('size-3 rounded-full bg-gray-300', {
+					'bg-green-500': nowEvent === true,
+					'bg-red-500': nowEvent === false,
+				})}></div>
+		</>
+	);
+};
