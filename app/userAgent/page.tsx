9948987d130566c917b1ab018d3124913a13e9d8
saemii-24 <hims2419@gmail.com>
2025-01-22 21:54:45 +0900

    Test: middleware test

--- app/userAgent/page.tsx
+++
@@ -1,8 +0,0 @@
-import {NextRequest, userAgent} from 'next/server';
-
-export default function UserAgent(request: NextRequest) {
-	const {device} = userAgent(request);
-	const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
-	console.log(viewport);
-	return <div>userAgent</div>;
-}
--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,18 @@ import {NextRequest, NextResponse, userAgent} from 'next/server';
 export function middleware(request: NextRequest) {
 	const {device} = userAgent(request);
 
+	// 모바일 디바이스일 때만 리디렉션 처리
 	if (device.type === 'mobile') {
 		const currentPath = request.nextUrl.pathname;
 		if (currentPath !== '/mobile-home') {
 			return NextResponse.redirect(new URL('/mobile-home', request.url));
 		}
 	}
+
+	// PC 디바이스일 경우 다음으로 진행한다.
+	return NextResponse.next();
 }
+
 export const config = {
 	matcher: '/', // 안쓰면 다른 경로까지 확인함
 };
