a344712cac62cb81bba7526cb0efca8d3fe05ada
saemii-24 <hims2419@gmail.com>
2025-01-25 23:50:57 +0900

    temp: tailwind-4 css variable

--- app/tailwind-4/page.tsx
+++ app/tailwind-4/page.tsx
@@ -5,12 +5,16 @@ export default function TailwindFour() {
 				<button className='px-4 py-2 bg-red-400 ring'>버튼입니다</button>
 				<button className='px-4 py-2 bg-blue-400 ring-1'>버튼입니다</button>
 				<button className='px-4 py-2 bg-green-400 ring-3'>버튼입니다</button>
+				<button className='px-4 py-2 bg-lime ring-3'>버튼입니다</button>
+				<button className='px-4 py-2 text-lime ring-3'>버튼입니다</button>
 
 				<input
 					type='text'
 					placeholder='이것이 placeholder'
 					className='border text-red-600 '
 				/>
+
+				<div className='size-20 bg-lime animate-fade-in-scale '></div>
 
 				<div className='size-10 border bg-red-300 text-purple-600 '>히히</div>
 			</div>
