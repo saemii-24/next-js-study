566e18842a4f7a1838d9119f4b5bdb01f56b8247
saemii-24 <hims2419@gmail.com>
2025-01-26 23:57:41 +0900

    feat: tailwind 4.0 container queries

--- app/tailwind-4/page.tsx
+++ app/tailwind-4/page.tsx
@@ -1,4 +1,11 @@
+'use client';
+import {useState} from 'react';
+import {cn} from 'utils/cn';
+
 export default function TailwindFour() {
+	// 상태 관리: 박스 크기 토글
+	const [isSmall, setIsSmall] = useState(false);
+
 	return (
 		<div className='h-dvh w-full p-6'>
 			<div className='flex flex-col gap-10'>
@@ -14,9 +21,27 @@ export default function TailwindFour() {
 					className='border text-red-600 '
 				/>
 
-				<div className='size-20 bg-lime animate-fade-in-scale '></div>
+				<div className='size-20 bg-lime animate-scale-up '></div>
 
 				<div className='size-10 border bg-red-300 text-purple-600 '>히히</div>
+
+				{/* 클릭 시 컨테이너 크기 변경*/}
+				<div
+					className={cn('@container bg-lime cursor-pointer ', {
+						'w-[100px]': isSmall,
+						'w-[200px]': !isSmall,
+					})}
+					onClick={() => {
+						setIsSmall(!isSmall);
+					}}>
+					{/* 텍스트: 컨테이너 크기에 따라 글씨 색이 변경됨 */}
+					<div
+						className={`text-center @sm:text-xs  @sm:text-red-500 @md:text-blue-500 @md:text-2xl text-black`}>
+						{isSmall
+							? '컨테이너가 작아져, 글씨가 작아졌습니다.'
+							: '컨테이너가 커져 글씨가 커졌습니다.'}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
