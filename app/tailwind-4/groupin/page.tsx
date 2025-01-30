9b917c08ca5724ccdf23b746921680ec14bf4c35
saemii-24 <hims2419@gmail.com>
2025-01-30 22:56:57 +0900

    Feat: group-name and not

--- app/tailwind-4/groupin/page.tsx
+++ app/tailwind-4/groupin/page.tsx
@@ -21,6 +21,24 @@ export default function Groupin() {
 					</div>
 				</div>
 			</div>
+
+			<Container.SubTitle>group에 이름주기</Container.SubTitle>
+			<ul role='list' className=' mt-3 space-y-2 **:cursor-pointer '>
+				<li className='group/item p-4 border'>
+					<span className='text-gray-900'>아이템 1</span>
+					<button className='group-hover/item:text-blue-500'>수정</button>
+				</li>
+
+				<li className='group/item p-4 border'>
+					<span className='text-gray-900'>아이템 2</span>
+					<button className='group-hover/item:text-blue-500'>수정</button>
+				</li>
+
+				<li className='group/edit p-4 border'>
+					<span className='text-gray-900'>아이템 3</span>
+					<button className='group-hover/edit:text-red-500'>삭제</button>
+				</li>
+			</ul>
 		</Container>
 	);
 }
--- app/tailwind-4/not/page.tsx
+++ app/tailwind-4/not/page.tsx
@@ -4,15 +4,22 @@ export default function Not() {
 	return (
 		<Container>
 			<Container.Title>not</Container.Title>
-			<div className='not-hover:opacity-75 bg-blue-500 p-4 text-white'>
-				마우스를 올리지 않으면 투명도 75%
-			</div>
-			<div className='not-motion-reduce:animate-bounce p-4 bg-green-500 text-white'>
-				모션 감소 설정이 아닐 때만 bounce 애니메이션 적용
-			</div>
-
-			<div className='not-supports-backdrop-blur:bg-gray-700 bg-white backdrop-blur-md p-4'>
-				backdrop-blur을 지원하지 않으면 bg-gray-700 적용
+			<div className='space-y-5'>
+				<div className='not-hover:opacity-75 bg-blue-500 p-4 text-white cursor-pointer'>
+					마우스를 올리지 않으면 투명도 75%
+				</div>
+				<label className='flex items-center space-x-2'>
+					<input type='checkbox' id='apple' className='hidden peer' />
+					<div className='peer-not-checked:bg-red-500 peer-checked:bg-green-500 text-white px-4 py-2 cursor-pointer'>
+						체크하면 초록색, 해제하면 빨간색
+					</div>
+				</label>
+				<div className='not-motion-reduce:animate-bounce p-4 bg-green-500 text-white'>
+					모션 감소 설정이 아닐 때만 bounce 애니메이션 적용
+				</div>
+				<div className='not-supports-backdrop-blur:bg-gray-700 bg-white backdrop-blur-md p-4 text-white'>
+					backdrop-blur을 지원하지 않으면 bg-gray-700 적용
+				</div>
 			</div>
 		</Container>
 	);
