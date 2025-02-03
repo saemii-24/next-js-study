9101f4a81c30a45927d69b7a480c3c8fa9200978
saemii-24 <hims2419@gmail.com>
2025-02-03 23:58:00 +0900

    Temp: clone layout

--- app/clone/layout.tsx
+++ app/clone/layout.tsx
@@ -1,15 +1,28 @@
 'use client';
-import React, {ReactElement} from 'react';
+import React, {
+	ReactNode,
+	isValidElement,
+	cloneElement,
+	Children,
+	ReactElement,
+} from 'react';
 
 type LayoutProps = {
-	children: ReactElement<any>;
+	children: ReactNode;
 };
 
 const Layout = ({children}: LayoutProps) => {
 	return (
 		<div>
 			<div>레이아웃에 추가된 제목</div>
-			{React.cloneElement(children, {someProp: '추가된 값'})}
+			{Children.map(children, (child) => {
+				if (isValidElement<{someProp?: string}>(child)) {
+					return cloneElement(child as ReactElement<{someProp?: string}>, {
+						someProp: '추가된 값',
+					});
+				}
+				return child;
+			})}
 		</div>
 	);
 };
--- app/clone/page.tsx
+++ app/clone/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import React from 'react';
 
+// 'someProp'이 옵션인 props 타입 정의
 const Clone = ({someProp}: {someProp?: string}) => {
-	console.log(someProp);
+	console.log(someProp); // 이제 '추가된 값'이 출력될 것입니다
 	return (
 		<div>
 			<div>자식 컴포넌트에 내려주는 값: {someProp}</div>
