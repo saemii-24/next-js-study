92088e84c05a453a447e8e0442fc8bc173783160
saemii-24 <hims2419@gmail.com>
2025-02-06 22:53:48 +0900

    feat: context

--- app/clone/layout.tsx
+++ app/clone/layout.tsx
@@ -1,22 +1,35 @@
 'use client';
 
 import Container from '@/components/Container';
-import {createContext} from 'react';
+import {createContext, useState} from 'react';
 
 // Context 생성
 export const LayoutContext = createContext('기본값 - 레이아웃');
+export const LayoutIdContext = createContext({
+	id: 1,
+	increase: () => {},
+	decrease: () => {},
+});
 
 export default function Layout({children}: {children: React.ReactNode}) {
+	const [layoutId, setLayoutId] = useState(1);
+
+	const increase = () => setLayoutId((prev) => prev + 1);
+	const decrease = () => setLayoutId((prev) => Math.max(0, prev - 1)); // 0 이하로 내려가지 않도록 설정
+
 	return (
 		<>
 			<Container>
 				<Container.Title>CloneElement</Container.Title>
+				<button
+					onClick={increase}
+					className='cursor-pointer p-2 bg-blue-500 text-white rounded'>
+					(layout.tsx) 숫자가 증가해요!
+				</button>
+				<LayoutIdContext.Provider value={{id: layoutId, increase, decrease}}>
+					{children}
+				</LayoutIdContext.Provider>
 			</Container>
-
-			{/* 위에서 정의한 '하이'를 덮어씀 */}
-			<LayoutContext.Provider value={'직접 내려준 값 - 레이아웃'}>
-				{children}
-			</LayoutContext.Provider>
 		</>
 	);
 }
--- app/clone/page.tsx
+++ app/clone/page.tsx
@@ -4,16 +4,38 @@ import {useContext} from 'react';
 import {TemplateContext} from './template';
 import {LayoutContext} from './layout';
 import Container from '@/components/Container';
+import {useRouter} from 'next/navigation';
 
 export default function Clone() {
 	const templateContext = useContext(TemplateContext);
 	const layoutContext = useContext(LayoutContext);
+	const router = useRouter();
+
 	return (
 		<Container>
 			<div className='font-bold'>template.tsx에서 받은 값</div>
 			<div>{templateContext}</div>
+
 			<div className='font-bold'>layout.tsx에서 받은 값</div>
 			<div>{layoutContext}</div>
+
+			<div className='mt-3 text-xl font-semibold'>이동하기</div>
+			{new Array(9).fill(0).map((_, index) => (
+				<div key={index} className='p-4 border border-gray-300 rounded-lg'>
+					<div className='flex justify-between items-center'>
+						<div className='flex-1'>{index + 1}</div>
+						<div className='flex-shrink-0'>
+							<button
+								onClick={() => {
+									router.push(`/clone/${index + 1}`);
+								}}
+								className='hover:bg-gray-50 cursor-pointer inline-flex items-center px-2 py-1 border border-gray-300 text-gray-700 rounded-md hover:text-gray-800'>
+								이동
+							</button>
+						</div>
+					</div>
+				</div>
+			))}
 		</Container>
 	);
 }
