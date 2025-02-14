8ae85ec38c8328e5620bde539ecf69fa79341ef9
saemii-24 <hims2419@gmail.com>
2025-02-14 23:14:38 +0900

    feat: fetch-error

---
+++ app/error/fetch-error/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+export default function ErroBoundary({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
+	return (
+		<div>
+			<h2>fetch중 에러가 발생했습니다.</h2>
+
+			<button
+				className='h-10 px-10 rounded-lg bg-red-500 text-white cursor-pointer'
+				onClick={() => reset()}>
+				재시도
+			</button>
+		</div>
+	);
+}
---
+++ app/error/fetch-error/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+import {useEffect, useState} from 'react';
+
+export default function Example() {
+	const [error, setError] = useState<Error | null>(null);
+
+	const fetchError = async () => {
+		try {
+			const response = await fetch('/api/error', {
+				method: 'POST',
+			});
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				throw new Error(errorData.message || `서버 오류: ${response.status}`);
+			}
+		} catch (err) {
+			//instance of = 객체가 특정 클래스의 인스턴스인지 확인한다.
+			if (err instanceof Error) {
+				setError(err);
+			}
+		}
+	};
+
+	useEffect(() => {
+		if (error) throw error; // 렌더링 과정에서 에러 발생
+	}, [error]);
+
+	return (
+		<button
+			className='bg-pink-500 cursor-pointer hover:bg-pink-500/70 text-white p-2 rounded-md'
+			onClick={fetchError}>
+			fetch 에러 발생
+		</button>
+	);
+}
