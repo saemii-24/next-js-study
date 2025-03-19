65acb04340b5635eddf397e8e76d8f0d1274a809
saemii-24 <hims2419@gmail.com>
2025-03-19 23:51:13 +0900

    Feat: Next15 Form

--- app/next-form/_components/TodoForm.tsx
+++ app/next-form/_components/TodoForm.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import Form from 'next/form';
+import {useActionState} from 'react';
 
 const TodoForm = () => {
+	const [todo, formAction, isPending] = useActionState(createTodo, null);
+
+	async function createTodo(formData: FormData): Promise<any> {
+		const title = formData.get('title');
+
+		const response = await fetch('/api/todo', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				date: '2025-03-19',
+				title: title,
+			}),
+		});
+		return response.json();
+	}
+
 	return (
-		<Form action='/data'>
+		<Form action={formAction}>
 			<label htmlFor='title'>해야할 일</label>
 			<input name='title' />
-			<button type='submit'>Submit</button>
+			<button type='submit' disabled={isPending}>
+				{isPending ? 'Submitting...' : 'Submit'}{' '}
+			</button>
 		</Form>
 	);
 };
